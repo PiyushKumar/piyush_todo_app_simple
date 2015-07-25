ee20409db70fde30522be07af9e8ce54a10c48f9
Piyush Kumar <kuma0177@gmail.com>
2015-07-25 16:33:00 -0700

    Adding form_for in model object, and adding messages for success and failures.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,23 @@
 class TodosController < ApplicationController
   def index
     @todo_items = Todo.all
+    #render :nothing => true
+    @new_todo = Todo.new
+
   end
 
   def delete
     #delete logic goes in here
   end
+
+  def add
+    todo = Todo.create(:todo_item => params[:todo][:todo_item])
+    unless todo.valid?
+      flash[:error] = todo.errors.full_messages.join("<br>").html_safe
+    else
+      #set flash[:success] to "Todo added successfully"
+    end
+    redirect_to :action => 'index'
+  end
+
 end
