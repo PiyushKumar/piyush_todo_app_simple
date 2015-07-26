ed48e66e8e8bde22f633898bebaafe864c45a11d
Piyush Kumar <kuma0177@gmail.com>
2015-07-25 19:27:21 -0700

    added methods in the completed_todo.rb migrate script to create apropriate columns when migrating to DB.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725235011) do
+ActiveRecord::Schema.define(version: 20150726021528) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "todo_item"
