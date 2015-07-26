b8523abf70fdb828975794bf85623afe7c3dd3cc
Piyush Kumar <kuma0177@gmail.com>
2015-07-25 18:51:45 -0700

    Adding features in index.html and routes.rb to make sure that the complete check box and complete button is displayed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'todos/index'
   get "/todos/delete"
   match "todos/add" => "todos#add", :via => :post
+  match 'todos/complete' => 'todos#complete', :via => :post
 end
